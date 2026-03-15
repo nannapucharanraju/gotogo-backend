7e44540c58be40275feda2b0fad04d1cc920da60
Charan <nannapucharanraju@gmail.com.com>
2026-03-15 22:02:21 +0530

    fix email smtp port for render

--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS instead of SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
